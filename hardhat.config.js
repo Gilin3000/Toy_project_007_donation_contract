235ddd8b0e2f89540068eeb1507b6bd538549817
Gilin3000 <lotus0110@nate.com>
2023-03-07 13:57:58 +0000

    feat : hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
 
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: `https://ethereum-goerli-rpc.allthatnode.com/${process.env.ALLTHATNODEAPI}`,
+      accounts: [`${process.env.PRIVATE_KEY}`],
+    }
+  }
 };
